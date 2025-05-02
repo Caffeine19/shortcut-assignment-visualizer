b951c7ddda8edd77fa8d5fd1a5176cda90b9a1fb
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2025-05-03 03:33:08 +0800

    :tada: init project

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from 'solid-js'
+const App: Component = () => {
+  return (
+    <>
+      <div
+        style={{
+          '-webkit-app-region': 'no-drag'
+        }}
+      ></div>
+    </>
+  )
+}
+
+export default App
