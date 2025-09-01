ac326db9c55346f2b4ce0a40f8819dca0f9eb70b
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2025-09-01 14:33:01 +0800

    🔧 update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
-import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier';
 import tseslint from '@electron-toolkit/eslint-config-ts';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 import eslintPluginSolid from 'eslint-plugin-solid';
 
 export default tseslint.config(
   { ignores: ['**/node_modules', '**/dist', '**/out'] },
   tseslint.configs.recommended,
   eslintPluginSolid.configs['flat/typescript'],
-  eslintConfigPrettier,
+  skipFormatting,
 
   {
     rules: {
