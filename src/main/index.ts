54d4ed49639408544d43fa80283bf0529042da80
UnnecTrycat <108726581+Caffeine19@users.noreply.github.com>
2026-06-09 14:50:22 +0800

    :bug: Support zoom

--- src/main/index.ts
+++ src/main/index.ts
@@ -66,7 +66,7 @@ app.whenReady().then(() => {
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window);
+    optimizer.watchWindowShortcuts(window, { zoom: true });
   });
 
   // IPC test
