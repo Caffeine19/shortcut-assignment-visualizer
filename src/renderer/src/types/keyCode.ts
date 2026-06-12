80bf39ca635867566050d62af346099542b1a48c
UnnecTrycat <108726581+Caffeine19@users.noreply.github.com>
2026-06-12 13:28:38 +0800

    :sparkles: Add arrow key

--- src/renderer/src/types/keyCode.ts
+++ src/renderer/src/types/keyCode.ts
@@ -79,4 +79,10 @@ export enum KeyCode {
   COMMA = ',',
   PERIOD = '.',
   SLASH = '/',
+
+  // Arrow keys
+  ARROW_UP = 'arrow_up',
+  ARROW_DOWN = 'arrow_down',
+  ARROW_LEFT = 'arrow_left',
+  ARROW_RIGHT = 'arrow_right',
 }
